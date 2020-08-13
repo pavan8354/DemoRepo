8c9e99fe33441b9f0f214dd53bb1c1b207b6831d
username <pavansdmitmech@gmail.com>
2020-08-13 15:39:58 +0530

    pushing the test code

---
+++ src/main/java/Static_method/Testing_push.java
@@ -0,0 +1,42 @@
+package Static_method;
+
+public class Testing_push {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Testing_push obj = new Testing_push();
+		obj.test();
+		obj.test1(20);
+		obj.test2(20, 30);
+		
+	}
+	
+	public static void main(int q) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello----->" +q);
+		
+			}
+	public static void main(float q) {
+		// TODO Auto-generated method stub
+		
+			}
+	
+	public void test() {
+		System.out.println("Test case with------zero param");
+
+	}
+	
+public  void test1(int i)   {		
+		System.out.println("Test case with------first param");			
+		System.out.println(i);
+}
+	
+public void test2(int i ,int j) {
+	
+	System.out.println("Test case with------second param");
+	System.out.print(j);
+	
+}
+
+}
